9f740604d450828dfa0a0f2292cd0e2b1ba1280d
Paul Mathews <pfcmathews@gmail.com>
2025-12-01 18:32:53 +1300

    one part one

--- cmd/one/main.go
+++ cmd/one/main.go
@@ -1,8 +1,61 @@
 // binary one solves the puzzle for the first of December.
 package main
 
-import "log"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-	log.Fatal("not implemented")
+	input, err := read()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("part one: %d\n", partOne(input))
+}
+
+func partOne(turns []int) int {
+	var (
+		pos   = 50
+		zeros = 0
+	)
+	for _, t := range turns {
+		pos = (pos + t) % 100
+		if pos == 0 {
+			zeros++
+		}
+	}
+	return zeros
+}
+
+// read reads the input from stdin, as a list of integers: right rotations are
+// positive, left negative.
+func read() ([]int, error) {
+	var (
+		scan    = bufio.NewScanner(os.Stdin)
+		results []int
+	)
+	for scan.Scan() {
+		line := scan.Text()
+		num, err := strconv.Atoi(line[1:])
+		if err != nil {
+			return nil, err
+		}
+		switch line[0] {
+		case 'L':
+			num = -num
+		case 'R':
+			// all good
+		default:
+			return nil, fmt.Errorf("bad line: %q", line)
+		}
+		results = append(results, num)
+	}
+	if err := scan.Err(); err != nil {
+		return nil, err
+	}
+	return results, nil
 }
