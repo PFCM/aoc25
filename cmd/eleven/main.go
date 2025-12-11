d7bc61b4ace4f0b26b8e6241494641009cd372d8
Paul Mathews <pfcmathews@gmail.com>
2025-12-11 20:54:55 +1300

    11.2 it's fast

--- cmd/eleven/main.go
+++ cmd/eleven/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -12,12 +13,70 @@ import (
 	"github.com/pfcm/aoc25"
 )
 
+var (
+	oneFlag = flag.Bool("one", true, "whether or not to do part 1")
+)
+
 func main() {
+	flag.Parse()
+
 	graph, err := read(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
-	aoc25.PrintTiming("Part one", func() int { return partOne(graph) })
+	if *oneFlag {
+		aoc25.PrintTiming("Part one", func() int { return partOne(graph) })
+	}
+	aoc25.PrintTiming("Part two", func() int { return partTwo(graph) })
+}
+
+func partTwo(d *devices) int {
+	// In the input there's 318388105768112962 paths between
+	// svr and out, so we need to be a little bit clever.
+	sorted := topo(d)
+
+	find := func(name string) int {
+		i := -1
+		for j, n := range d.names {
+			if n == name {
+				i = j
+				break
+			}
+		}
+		if i == -1 {
+			panic(fmt.Sprintf("no %q", name))
+		}
+		return i
+	}
+
+	svr := find("svr")
+	dac := find("dac")
+	fft := find("fft")
+
+	paths := make([]int, len(d.names))
+	between := func(from, to int) int {
+		for i := range paths {
+			paths[i] = 0
+		}
+		paths[from] = 1
+		for _, node := range sorted {
+			for _, neighbour := range d.edges[node] {
+				paths[neighbour] += paths[node]
+			}
+		}
+		return paths[to]
+	}
+	s2d := between(svr, dac)
+	d2f := between(dac, fft)
+
+	s2f := between(svr, fft)
+	f2d := between(fft, dac)
+
+	d2o := between(dac, len(d.names)-1)
+	f2o := between(fft, len(d.names)-1)
+
+	// fmt.Printf("s2d: %d, d2f: %d, s2f: %d, f2d: %d, d2o: %d, f2o: %d\n", s2d, d2f, s2f, f2d, d2o, f2o)
+	return (s2d * d2f * f2o) + (s2f * f2d * d2o)
 }
 
 func partOne(d *devices) int {
@@ -32,7 +91,22 @@ func partOne(d *devices) int {
 	if start == -1 {
 		panic("where are you?")
 	}
+	sorted := topo(d)
+	// Now we have them in order, we run through and build a running count
+	// of how many ways there are to get to each node.
+	paths := make([]int, len(d.names))
+	paths[start] = 1
+	for _, node := range sorted {
+		for _, neighbour := range d.edges[node] {
+			paths[neighbour] += paths[node]
+		}
+	}
 
+	return paths[len(paths)-1]
+}
+
+func topo(d *devices) []int {
+	// Kahn's algorithm for topological sort.
 	inDegrees := make([]int, len(d.names))
 	for _, es := range d.edges {
 		for _, to := range es {
@@ -59,18 +133,7 @@ func partOne(d *devices) int {
 			}
 		}
 	}
-
-	// Now we have them in order, we run through and build a running count
-	// of how many ways there are to get to each node.
-	paths := make([]int, len(d.names))
-	paths[start] = 1
-	for _, node := range sorted {
-		for _, neighbour := range d.edges[node] {
-			paths[neighbour] += paths[node]
-		}
-	}
-
-	return paths[len(paths)-1]
+	return sorted
 }
 
 type devices struct {
